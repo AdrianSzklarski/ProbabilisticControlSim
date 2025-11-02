39ce17ffd01bb7744925414dc229ffba94f88efc
AdrianSzklarski <adrian_it@o2.pl>
2025-11-03 00:05:23 +0100

    Add main.py starter file

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Program name: Probabilistic LQR Control / Demo
+Author: Adrian Szklarski
+Year: 2025
+License: Free for non-commercial use
+
+Description:
+Startup file for PyCharm (Run → main.py)
+Currently under development — this script will serve as the program launcher.
+It currently runs a simple demo placeholder.
+"""
+
+
+def main():
+    print("Program in progress.")
+
+
+if __name__ == "__main__":
+    main()
